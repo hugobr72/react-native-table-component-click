f247ec7dac50d6ea1bd868d924ee00f62b1cb087
Hugo Barbosa Rodrigues <hugobrod72@gmail.com>
2023-05-04 17:42:19 -0300

    initial commit

---
+++ components/cell.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Data, DefaultProps } from './types';
+
+export const Cell: React.FC<DefaultProps<Data>> = props => {
+  const { data, width, height, flex, style, textStyle, borderStyle, ...rest } = props;
+
+  const textDom = React.isValidElement(data) ? (
+    data
+  ) : (
+    <Text style={[textStyle, styles.text]} {...rest}>
+      {data}
+    </Text>
+  );
+  const borderTopWidth = (borderStyle && borderStyle.borderWidth) || 0;
+  const borderRightWidth = borderTopWidth;
+  const borderColor = (borderStyle && borderStyle.borderColor) || '#000';
+
+  return (
+    <TouchableOpacity
+      onPress={() => props.setClick(textDom)}
+      style={[
+        {
+          borderTopWidth,
+          borderRightWidth,
+          borderColor
+        },
+        styles.cell,
+        width && { width },
+        height && { height },
+        flex && { flex },
+        !width && !flex && !height && !style && { flex: 1 },
+        style
+      ]}
+    >
+      {textDom}
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  cell: { justifyContent: 'center' },
+  text: { backgroundColor: 'transparent' }
+});
